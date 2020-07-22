f412d5c4f4b27fd4462c5c147d23bfc7e50b8b2b
GenkiSugiyama <sgym0318marcos@gmail.com>
2020-07-22 23:41:05 +0900

    [~P219]5-14まで ※tasks_spec.rbの82行目〜のエラーを期待してるのにエラーなく登録できてしまいテストが通らない

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,64 +3,40 @@ require 'rails_helper'
 # letを使って変数（のようなもの）に値を代入する
 # let(定義名) {定義の内容}
 # letで定義したものはその外側でも内側でも使用することができる
+# letは呼び出されて初めて実行されるが、呼び出されなければ実行されない
+# 呼び出しはしないが、定義内容を実行したい場合は「let!」を使用する
 
 describe 'タスク管理機能', type: :system do
+
+  # 大前提の準備としてユーザーAとユーザーBの変数を定義しておく
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  # 詳細画面表示テストでしか呼び出ししていないが、一覧表示テストでも必要なため「let!」で定義している
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  # it内の動作を共通化するために「shared_examples」を使用する
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe ('一覧表示機能') do
 
-    # 大前提の準備としてユーザーAとユーザーBの変数を定義しておく
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
-
-    before do
-      # ユーザーAを作成しておく（letで共通化するため個別作成は不要となりコメントアウト）
-      # user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      # ↑のように属性を指定し任意の値を入れることもできる
-      # ↓のように記述するとuser_aの属性は..factories/users.rbに記述された属性が入る
-      # user_a = FactoryBot.create(:user)
-
-      # 作成者がユーザーAであるタスクを作成しておく
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-
-      # 同階層のdescribe/contextで共通する処理はその一つ上階層のbeforeの中に記述することで処理の共通化が可能
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザーAがログインしている時' do
-      # 一つ上の階層のbeforeの中に共通化したため↓コメントアウト
-      # before do
-      #   # ログイン画面にアクセスする
-      #   # 「visit [URL]」で特定のURLにアクセスする
-      #   visit login_path
-      #   # メールアドレスを入力する
-      #   # <label>のname属性を指定することでそのlabelと結び付いたテキストフィールドに値を入力する
-      #   fill_in 'メールアドレス', with: 'a@example.com'
-      #   # パスワードを入力する
-      #   fill_in 'パスワード', with: 'password'
-      #   # 「ログインする」ボタンをおす
-      #   click_button 'ログインする'
-      # end
 
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク' # 「最初のタスク」という内容を画面に期待する的な
-      end
+      # 共通化されたitをshared_exampleを利用する形に書き換える
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしている時' do
-      # before do
-      #   # ユーザーBを作成しておく（letで共通化するため個別作成は不要となりコメントアウト）
-      #   # FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
-      #   # ユーザーBでログインする（一つ上の階層のbeforeの中に共通化したため↓コメントアウト）
-      #   # visit login_path
-      #   # fill_in 'メールアドレス', with: 'b@example.com'
-      #   # fill_in 'パスワード', with: 'password'
-      #   # click_button 'ログインする'
-      # end
 
       let(:login_user) { user_b }
 
@@ -71,4 +47,48 @@ describe 'タスク管理機能', type: :system do
       end
     end
   end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしている時' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      # 共通化されたitをshared_exampleを利用する形に書き換える
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+
+    before do
+      visit new_task_path
+      fill_in '名称', with: task_name
+      click_button '登録する'
+    end
+
+    context '新規作成画面で名称を入力した時' do
+      let(:task_name) { '新規作成のテストを書く' }
+
+      it '正常に登録される' do
+        # have_selectorを使うとhtml内の要素をセレクタで指定できる
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+
+    context '新規作成画面で名称を入力しなかった時' do
+      let(:task_name) { '' }
+
+      it 'エラーとなる' do
+        # withinで指定した要素の中のみで期待した要素が存在するかをチェックする
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
+      end
+    end
+  end
+
 end
